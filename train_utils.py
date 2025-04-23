57603bf22ed47e159dc8e51c5c2dc64a94b0a7ae
“maximoskp” <maximoskalpap@gmail.com>
2025-04-23 08:29:21 +0300

    different curricula

--- train_utils.py
+++ train_utils.py
@@ -2,53 +2,80 @@ import torch
 import random
 from tqdm import tqdm
 from data_utils import compute_normalized_token_entropy
+import random
 # TODO:
 # validation loop
 # save model
 # write results to excel
 # integrate compute_normalized_token_entropy and perplexity
 
-def apply_structured_masking(harmony_tokens, mask_token_id, step_idx, time_sigs, incremental=False):
+def apply_structured_masking(harmony_tokens,
+    mask_token_id,
+    step_idx,
+    time_sigs,
+    curriculum_type='no'):
     """
-    harmony_tokens: (B, 256) - original ground truth tokens
+    harmony_tokens: (B, time_step) - original ground truth tokens
     mask_token_id: int - ID for the special <mask> token
     step_idx: int - 0, 1, 2, etc.
     time_sigs: batch of 16-item binary encodings of time signature for each item
+    curriculum_type: how to progress with masking
+    - 'no': all tokens are masked and the model needs to learn to unmask all
+    - 'random': an increasing number of tokens is masked and the model needs to learn to unmask all
+    - 'ts_blank': an increasing number of ts-based tokens is masked
+    - 'ts_incr': an increasing number of ts-based tokens is masked while previous are unmasked
 
     Returns:
-        masked_harmony: (B, 256) with some tokens replaced with <mask>
-        target_harmony: (B, 256) with -100 at positions we do NOT want loss
+        masked_harmony: (B, time_steps) with some tokens replaced with <mask>
+        target_harmony: (B, time_steps) with -100 at positions we do NOT want loss
     """
     B, T = harmony_tokens.shape
 
-    # Create masked version
+    # Create masked version that will serve as the input
     masked_harmony = torch.full_like( harmony_tokens, mask_token_id )
 
-    # Create target (loss computed only on masked positions)
+    # Create target. Loss computed only on masked positions that we care about learning
+    # In incremental learning, we care to learn only a portion of the masked input tokens,
+    # while for other masked tokens we don't care.
     target = harmony_tokens.clone()
     device = harmony_tokens.device
-    mask = torch.full_like( harmony_tokens, 0, dtype=torch.bool, device=device )
-    # mask = torch.zeros((T,), dtype=torch.bool, device=device)  # start with all masked
-    # Expand to batch
-    # mask = mask.unsqueeze(0).expand(B, -1)  # shape (B, 256)
-    if incremental and step_idx > 0:
-        input_mask = torch.full_like( harmony_tokens, 0, dtype=torch.bool, device=device )
+    # assume that not tokens need to be masked for learning
+    target_to_learn = torch.full_like(
+        harmony_tokens,
+        curriculum_type=='random' or curriculum_type=='no',
+        dtype=torch.bool,
+        device=device
+    )
+    if curriculum_type == 'ts_incr' and step_idx > 0:
+        # some tokens need to be revealed in the input for incremental learning
+        input_unmask = torch.full_like( harmony_tokens, 0, dtype=torch.bool, device=device )
 
-    for i in range(B):
-        # get ts num and den
-        ts_num = torch.nonzero(time_sigs[i, :14])[0] + 2
-        ts_den = torch.nonzero(time_sigs[i, 14:])[0]*8 - 4
-        spacing_schedule = [ 4*ts_num*(ts_den//4) , 2*ts_num*(ts_den//4) , 1*ts_num*(ts_den//4) ]
-        spacing = spacing_schedule[step_idx] if step_idx < len(spacing_schedule) else 1
+    if curriculum_type == 'ts_incr' or curriculum_type == 'ts_blank':
+        for i in range(B):
+            # get ts num and den
+            ts_num = torch.nonzero(time_sigs[i, :14])[0] + 2
+            ts_den = torch.nonzero(time_sigs[i, 14:])[0]*8 - 4
+            spacing_schedule = [ 4*ts_num*(ts_den//4) , 2*ts_num*(ts_den//4) , 1*ts_num*(ts_den//4) ]
+            spacing = spacing_schedule[step_idx] if step_idx < len(spacing_schedule) else 1
 
-        # Get the indices that will remain unmasked for this step
-        mask[i, ::spacing] = True  # reveal tokens at spacing in target
-        if incremental and step_idx > 0:
-            spacing = spacing_schedule[step_idx-1] if step_idx-1 < len(spacing_schedule) else spacing_schedule[-1]
-            input_mask[i, ::spacing] = True  # reveal tokens at spacing in harmony input
-    target[~mask] = -100  # ignore tokens that were shown to the model
-    if incremental and step_idx > 0:
-        masked_harmony[input_mask] = harmony_tokens[input_mask]
+            # Get the indices that will remain unmasked for this step
+            target_to_learn[i, ::spacing] = True  # reveal tokens at spacing in target
+            if curriculum_type == 'ts_incr' and step_idx > 0:
+                spacing = spacing_schedule[step_idx-1] if step_idx-1 < len(spacing_schedule) else spacing_schedule[-1]
+                input_unmask[i, ::spacing] = True  # reveal tokens at spacing in harmony input
+    if curriculum_type == 'random':
+        step_ratios = {0: 0.6, 1: 0.3, 2: 0}
+        for i in range(B):
+            if step_idx < 2:
+                step_ratio = step_ratios[step_idx]
+                valid_indices = (harmony_tokens[i] != -1).nonzero(as_tuple=False).squeeze()
+                n_reveal = max(1, int(len(valid_indices) * step_ratio))
+                reveal_indices = random.sample(valid_indices.tolist(), n_reveal)
+                masked_harmony[i, reveal_indices] = harmony_tokens[i, reveal_indices]
+            target_to_learn = masked_harmony == mask_token_id
+    target[~target_to_learn] = -100  # ignore tokens that were shown to the model
+    if curriculum_type == 'ts_incr' and step_idx > 0:
+        masked_harmony[input_unmask] = harmony_tokens[input_unmask]
     return masked_harmony, target
 # end apply_structured_masking
 
@@ -68,7 +95,8 @@ def get_step_idx_mixed(epoch, max_epoch, max_step_idx):
 def train_with_curriculum(
     model, optimizer, trainloader, loss_fn, mask_token_id,
     epochs=10,
-    curriculum_type="linear",  # "linear" or "mixed"
+    curriculum_type='no',  # 'no', 'random', 'ts-based'
+    curriculum_steps='linear', # 'linear', mixed
     epochs_per_stage=2,
 ):
     device = next(model.parameters()).device
@@ -78,12 +106,12 @@ def train_with_curriculum(
         model.train()
 
         # Determine masking level
-        if curriculum_type == "linear":
+        if curriculum_steps == "linear":
             step_idx = get_step_idx_linear(epoch, epochs_per_stage, max_step_idx)
-        elif curriculum_type == "mixed":
+        elif curriculum_steps == "mixed":
             step_idx = get_step_idx_mixed(epoch, epochs, max_step_idx)
         else:
-            raise ValueError("Invalid curriculum type")
+            raise ValueError("Invalid curriculum steps")
 
         epoch_loss = 0.0
         running_loss = 0.
@@ -101,7 +129,8 @@ def train_with_curriculum(
                     harmony_gt,
                     mask_token_id,
                     step_idx,
-                    conditioning_vec
+                    conditioning_vec,
+                    curriculum_type
                 )
 
                 # Forward pass
